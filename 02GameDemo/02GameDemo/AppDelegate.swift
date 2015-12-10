f5f4a37ce781a1158d30fc36b55b7472c484b5fc
jgh33 <jgh@jghdeMac-Pro.mshome.net>
2015-12-10 09:47:39 +0800

    删除2-3，新增02
    用scene创建OS X gameDemo

---
+++ 02GameDemo/02GameDemo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  02GameDemo
+//
+//  Created by jgh on 15/12/10.
+//  Copyright (c) 2015年 jgh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
