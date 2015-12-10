44eacb51f123f61259560f09f423086170f815f2
jgh33 <jgh@jghdeMac-Pro.mshome.net>
2015-12-10 08:54:58 +0800

    增加第一课
    01-学习如何在xcode下创建工作空间

---
+++ Learn01WorkSpace/Learn01WorkSpace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Learn01WorkSpace
+//
+//  Created by jgh on 15/12/10.
+//  Copyright © 2015年 jgh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
